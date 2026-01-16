43bf3f0cc682b0e926ce6f87c46f7bf555a01b07
pim <pim@NixBTW>
2026-01-16 19:58:00 +0100

    Fixed some driver issues

--- modules/core/drivers.nix
+++ modules/core/drivers.nix
@@ -1,21 +1,28 @@
 {config, ...}: {
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
-    powerManagement = {
-      enable = false;
-      finegrained = false;
-    };
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      open = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
+      powerManagement = {
+        enable = false;
+        finegrained = false;
       };
-      intelBusId = "PCI:1:0:0";
-      nvidiaBusId = "PCI:0:2:0";
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        intelBusId = "PCI:1:0:0";
+        nvidiaBusId = "PCI:0:2:0";
+      };
+    };
+
+    graphics = {
+      enable = true;
+      enable32Bit = true;
     };
   };
 }
