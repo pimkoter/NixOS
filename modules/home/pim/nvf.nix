49e51d4f9f2cc31c83552b29c50fdcc934fe2a09
pim <pim@NixBTW>
2026-06-20 22:41:20 +0200

    Completely reworked NVF

--- modules/home/pim/nvf.nix
+++ modules/home/pim/nvf.nix
@@ -1,46 +1,23 @@
 {
   inputs,
-  config,
+  lib,
   ...
 }: {
   imports = [inputs.nvf.homeManagerModules.default];
-
   programs.nvf = {
     enable = true;
-
     settings.vim = {
-      vimAlias = true;
-      viAlias = true;
-      withNodeJs = true;
-      lineNumberMode = "number";
-      enableLuaLoader = true;
-      preventJunkFiles = true;
-      autocomplete.nvim-cmp.enable = true;
-      telescope.enable = true;
-
-      assistant = {
-        copilot = {
-          enable = true;
-          cmp.enable = true;
-        };
-
-        codecompanion-nvim = {
-          enable = true;
-          setupOpts = {
-            interactions = {
-              chat = {adapter = "copilot";};
-              inline = {adapter = "copilot";};
-            };
-          };
-        };
-      };
-
+      # --- OPTIONS ---
       options = {
-        tabstop = 4;
+        number = true;
+        relativenumber = true;
+        expandtab = true;
         shiftwidth = 2;
-        wrap = false;
+        tabstop = 2;
+        smartindent = true;
       };
 
+      # --- CLIPBOARD CONFIGURATION ---
       clipboard = {
         enable = true;
         registers = "unnamedplus";
@@ -50,49 +27,119 @@
         };
       };
 
+      # --- KEY MAPPINGS ---
+      globals.mapleader = " ";
+      keymaps = [
+        {
+          key = "<leader>cd";
+          mode = "n";
+          action = "<cmd>Ex<CR>";
+          silent = true;
+        }
+        {
+          key = "<leader>fw";
+          mode = "n";
+          action = "<cmd>FzfLua grep_cword";
+          silent = true;
+        }
+        {
+          key = "<leader>fr>";
+          mode = "n";
+          action = "<cmd>FzfLua resume";
+          silent = true;
+        }
+        {
+          key = "<leader>ff";
+          mode = "n";
+          action = "<cmd>FzfLua files<CR>";
+          silent = true;
+        }
+        {
+          key = "<leader>fo";
+          mode = "n";
+          action = "<cmd>FzfLua oldfiles<CR>";
+          silent = true;
+        }
+        {
+          key = "<leader>fb";
+          mode = "n";
+          action = "<cmd>FzfLua buffers<CR>";
+          silent = true;
+        }
+        {
+          key = "<leader>fg";
+          mode = "n";
+          action = "<cmd>FzfLua live_grep<CR>";
+          silent = true;
+        }
+        {
+          key = "gd";
+          mode = "n";
+          action = "<cmd>lua vim.lsp.buf.definition()<CR>";
+          silent = true;
+        }
+        {
+          key = "gr";
+          mode = "n";
+          action = "<cmd>lua vim.lsp.buf.references()<CR>";
+          silent = true;
+        }
+        {
+          key = "K";
+          mode = "n";
+          action = "<cmd>lua vim.lsp.buf.hover()<CR>";
+          silent = true;
+        }
+        {
+          key = "gl";
+          mode = "n";
+          action = "<cmd>lua vim.diagnostic.open_float()<CR>";
+          silent = true;
+        }
+        {
+          key = "<leader>nd";
+          mode = "n";
+          action = "<cmd>lua vim.diagnostic.goto_next()<CR>";
+          silent = true;
+        }
+        {
+          key = "<leader>pd";
+          mode = "n";
+          action = "<cmd>lua vim.diagnostic.goto_prev()<CR>";
+          silent = true;
+        }
+      ];
+      # --- AI INTEGRATION ---
+
+      # --- THEMING ---
+      theme = {
+        enable = true;
+        name = lib.mkForce "catppuccin";
+        style = lib.mkForce "auto";
+      };
+
+      # --- PLUGINS & LSP CONFIGURATION ---
       lsp = {
         enable = true;
         formatOnSave = true;
-        trouble.enable = true;
       };
-
+      binds.whichKey.enable = true;
+      filetree.nvimTree.enable = true;
+      git.gitsigns.enable = true;
+      autopairs.nvim-autopairs.enable = true;
+      comments.comment-nvim.enable = true;
+      autocomplete.blink-cmp.enable = true;
       languages = {
         enableFormat = true;
         enableTreesitter = true;
         nix.enable = true;
-        yaml.enable = true;
+        typescript.enable = true;
+        python.enable = true;
       };
-
-      visuals = {
-        nvim-web-devicons.enable = true;
-        fidget-nvim.enable = true;
-        indent-blankline.enable = true;
-        rainbow-delimiters.enable = true;
-      };
-
+      fzf-lua.enable = true;
       statusline.lualine = {
         enable = true;
-        theme = "base16";
       };
-
-      autopairs.nvim-autopairs.enable = true;
-      snippets.luasnip.enable = true;
-      filetree.neo-tree.enable = true;
-      notify.nvim-notify.enable = true;
-
-      git = {
-        enable = true;
-        gitsigns.enable = true;
-      };
-
-      utility = {
-        motion = {
-          hop.enable = true;
-          leap.enable = true;
-        };
-      };
-
-      comments.comment-nvim.enable = true;
     };
   };
 }
