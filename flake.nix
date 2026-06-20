7d46f529992edc6b93ff8117caa219a76cd1f769
pim <pim@NixBTW>
2026-06-20 14:12:19 +0200

    Fixed noctalia

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nvf = {
